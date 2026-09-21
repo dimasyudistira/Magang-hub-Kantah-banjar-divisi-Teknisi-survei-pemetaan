105bb99d20852c3dfe0cbb8532b3c8492b4507ff
Dimas Yudhistira <101631453+dimasyudistira@users.noreply.github.com>
2026-09-21 10:43:57 +0700

    Add files via upload

---
+++ script.js
@@ -0,0 +1,18 @@
+// Script untuk mengontrol menu mobile
+
+document.addEventListener('DOMContentLoaded', () => {
+    const btn = document.getElementById('mobile-menu-btn');
+    const menu = document.getElementById('mobile-menu');
+
+    btn.addEventListener('click', () => {
+        menu.classList.toggle('hidden');
+    });
+
+    // Menutup menu mobile saat salah satu link diklik
+    const mobileLinks = menu.querySelectorAll('a');
+    mobileLinks.forEach(link => {
+        link.addEventListener('click', () => {
+            menu.classList.add('hidden');
+        });
+    });
+});
